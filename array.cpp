0a98b7f9895a30cba915a71e742bbb6bfd951690
Archita1805 <148620281+Archita1805@users.noreply.github.com>
2024-08-04 11:05:02 +0530

    added array

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr1[]={1,2,3,4,5};
+    int arr2[10];
+    cout<<arr1[0]<<endl;
+    arr1[2]= 18;
+    cout<<arr1[2]<<endl;
+    arr2[1]=20;
+    cout<<arr2[1];
+}
